97b94a8b4bb251e99182a1e892631c4ea5ed5f01
Oliver Gasser <gasser@net.in.tum.de>
2018-03-21 21:22:29 +0100

    IPv6: eliminate asprintf compiler warnings

--- src/probe_modules/module_icmp6_echoscan.c
+++ src/probe_modules/module_icmp6_echoscan.c
@@ -35,7 +35,9 @@ probe_module_t module_icmp6_echoscan;
 int icmp6_echo_global_initialize(struct state_conf *conf)
 {
 	// Only look at received packets destined to the specified scanning address (useful for parallel zmap scans)
-	asprintf(&module_icmp6_echoscan.pcap_filter, "%s && ip6 dst host %s", module_icmp6_echoscan.pcap_filter, conf->ipv6_source_ip);
+	if (asprintf((char ** restrict) &module_icmp6_echoscan.pcap_filter, "%s && ip6 dst host %s", module_icmp6_echoscan.pcap_filter, conf->ipv6_source_ip) == -1) {
+		return 1;
+	}
 
 	return EXIT_SUCCESS;
 }
